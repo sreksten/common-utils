ac3246d3d798245314e24edf04559dbfbaa66010
Stefano Reksten <stefano.reksten@gmail.com>
2026-02-03 01:21:37 +0100

    Cache: moved to collections

--- src/test/java/com/threeamigos/common/util/implementations/injection/CacheClaudeUnitTest.java
+++ src/test/java/com/threeamigos/common/util/implementations/injection/CacheClaudeUnitTest.java
@@ -1,5 +1,6 @@
 package com.threeamigos.common.util.implementations.injection;
 
+import com.threeamigos.common.util.implementations.collections.Cache;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
