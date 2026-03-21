8570a8532b8aaf5baa955578b7ce5deac3c59c94
Stefano Reksten <stefano.reksten@gmail.com>
2026-03-21 20:07:30 +0100

    Client proxies - 5.4

---
+++ src/test/java/com/threeamigos/common/util/implementations/injection/cdi41tests/chapter5/par55injection/DependencyInjectionTest.java
@@ -0,0 +1,7 @@
+package com.threeamigos.common.util.implementations.injection.cdi41tests.chapter5.par55injection;
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName( "5.5 - Dependency Injection Test")
+public class DependencyInjectionTest {
+}
